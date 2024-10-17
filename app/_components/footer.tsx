926bb407daff2c8ef388c094bd2d7f4eae3dd32b
Éverton Alves <140563844+carloseverton96@users.noreply.github.com>
2024-10-17 15:15:35 -0300

    update footer.tsx

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -9,21 +9,21 @@ const FOOTER = () => {
         <footer className="flex justify-center items-center space-x-4 p-4 bg-gray-100">
         {/* Botão para Facebook */}
         <Button asChild variant="ghost" className="text-blue-600">
-          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.facebook.com/profile.php?id=100008324709881" target="_blank" rel="noopener noreferrer">
           <Image alt="auta de souza" src="/facebook.svg" width={20} height={20} className=""/>
           </a>
         </Button>
   
         {/* Botão para Instagram */}
         <Button asChild variant="ghost" className="text-pink-600">
-          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.instagram.com/cecrecristoredentor/" target="_blank" rel="noopener noreferrer">
           <Image alt="auta de souza" src="/instagram.svg" width={20} height={20} className=""/>
           </a>
         </Button>
   
         {/* Botão para WhatsApp */}
         <Button asChild variant="ghost" className="text-green-600">
-          <a href="https://wa.me/yourphonenumber" target="_blank" rel="noopener noreferrer">
+          <a href="https://wa.me/+5588999721897" target="_blank" rel="noopener noreferrer">
           <Image alt="auta de souza" src="/phone.svg" width={20} height={20} className=""/>
           </a>
         </Button>
